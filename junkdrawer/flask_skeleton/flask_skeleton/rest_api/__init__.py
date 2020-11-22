743284138f9b08f8a020c935b82b0f20996832e4
elegantmoose <mkouremetis16@gmail.com>
2020-11-22 15:02:44 -0500

    missed

---
+++ junkdrawer/flask_skeleton/flask_skeleton/rest_api/__init__.py
@@ -0,0 +1,57 @@
+from importlib import import_module
+import logging, logging.handlers
+import os
+
+from flask import Flask
+from yaml import safe_load
+
+
+CONFIG_FP_EV = "<CONFIG_FP_EV>"   # Name of environment variable that has config filepath
+TEST_FLAG_EV = "<TEST_FLAG_EV>"    # Name of environment variable that is the test flag
+TEST_ENDPOINTS_MODULE_EV = "<TEST_ENDPOINTS_MODULE_EV>"  #
+
+
+def create_app(config=None):
+    app = Flask("<app_name>")
+
+    # Use gunicorn logging mechanism
+    # (REF: https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f)
+    if __name__ != "__main__":
+        gunicorn_logger = logging.getLogger("gunicorn.error")
+        app.logger.handlers = gunicorn_logger.handlers
+        app.logger.setLevel(gunicorn_logger.level)
+
+    # --- Initialize App ---
+    CONFIG_FILE = os.environ.get(CONFIG_FP_EV)
+
+    if os.environ.get(TEST_FLAG_EV, default="False") != "True":
+        # -- production mode --
+        app.logger.critical("Production mode set.")
+        app.config["config"] = safe_load(open(CONFIG_FILE, 'r'))
+
+        # (any further initialization, maybe components/objects that REST
+        # API will use.)
+        #
+        #
+
+        # Load REST API
+        endpoint_modules = []
+        for module_name in app.config["endpoints"]:
+            module_path = "flask_skeleton.rest_api" + module_name
+            endpoint_modules.append(module_path)
+            module = import_module(module_path)
+            blueprint = getattr(module, module_name)   # Blueprint has same name as module
+            app.register_blueprint(blueprint)
+
+        app.logger.critical("<init messages>")
+    else:
+        # -- test mode --
+        app.logger.critical("Test mode set.")
+
+        # Add endpoints from module supplied through env variable
+        module = import_module(os.environ[TEST_ENDPOINTS_MODULE_EV])
+        module_name = os.environ[TEST_ENDPOINTS_MODULE_EV].split(".")[-1]
+        blueprint = getattr(module, module_name)   # Blueprint has same name as module
+        app.register_blueprint(blueprint)
+
+    return app
